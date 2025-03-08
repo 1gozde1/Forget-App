ac5fafd83936807a48ac7204eac13652e45aa83b
Gözde Hızır <goz.akkaya@gmail.com>
2025-03-08 23:17:41 +0300

    Update GitHub Actions workflow to set up Firebase environment variables and downgrade Node.js setup action

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,7 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -11,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-
 };
 
 const app = initializeApp(firebaseConfig);
